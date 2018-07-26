b8c3b73e8c1be3747a1e9642bfd1b62df9a57ff0
Thomas Santoyo <tommysantoyo@gmail.com>
2018-07-26 12:06:04 -0500

    layout is pretty much done

--- src/js/test.js
+++
@@ -1,9 +0,0 @@
-var toggle = document.getElementById("navbar-toggle");
-var navbar = document.getElementById("navbar");
-toggle.addEventListener("click", function () {
-   if(navbar.classList.contains("navbar-open")){
-      navbar.classList.remove("navbar-open");
-   } else {
-      navbar.classList.add("navbar-open");
-   }
-});
